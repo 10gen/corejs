584507034fb6e4c02b044b4caee21f11ba56d3b0
kristina <k@wooster.earthlink.net>
2008-06-24 15:33:27 -0400

    added versioning

--- util/doc.js
+++ util/doc.js
@@ -14,9 +14,9 @@ Util.Doc.toDb = function(file, javaFunc) {
     javaStatic("ed.doc.Generate", javaFunc, file);
 }
 
-Util.Doc.DbToHTML = function(out_dir) {
+Util.Doc.DbToHTML = function(out_dir, version) {
     javaStatic("ed.doc.Generate", "removeOldDocs", local.getRoot()+"/"+out_dir);
-    var d = db.doc.find();
+    var d = db.doc.find({version : version});
     while(d.hasNext()) {
         this.DbObjToHTML(d.next(), out_dir);
     }
@@ -24,4 +24,8 @@ Util.Doc.DbToHTML = function(out_dir) {
 
 Util.Doc.DbObjToHTML = function(obj, out_dir) {
     javaStatic("ed.doc.Generate", "toHTML", tojson(obj._index), out_dir);
+}
+
+Util.Doc.setVersion = function(version) {
+    javaStatic("ed.doc.Generate", "setVersion", version);
 }
