e510c0145d7190f095d1a0567fe172f0f96981ce
Ethan Glasser-Camp <ethan@10gen.com>
2008-07-07 14:10:05 -0400

    Document.

--- net/email.js
+++ net/email.js
@@ -1,3 +1,11 @@
+/**
+ *  Test whether a given string is a valid email address.
+ *  For a laugh, check http://ex-parrot.com/~pdw/Mail-RFC822-Address.html to see
+ *  how to support RFC822 email addresses. We don't do that; we just check for
+ *  basic email syntax.
+ *  @param {string} s the string to test
+ *  @returns {boolean}
+ */
 net.isEmail = function(s){
     return s.match(/^\w[\w\+\.]*@\w+(\.\w+)+$/);
 };
