492adb9afbcbde928c38d0ac3b6da157ddb6f5a1
Geir Magnusson Jr <geir@pthbbbbbb-2.local>
2008-07-07 04:43:35 -0400

    test_forms.js : fix.  not sure if this ever worked?

--- content/test/test_forms.js
+++ content/test/test_forms.js
@@ -2,4 +2,4 @@
 core.content.forms();
 
 f = new Forms.Form( { a : 1 , b : 2 } , "z_" );
-assert( f.text( "a" ).trim() == "<input  type name=\"z_a\"  value=\"1\" >" , f.text("a" ) );
+assert( f.text( "a" ).trim() == "<input  type=\"text\" name=\"z_a\"  value=\"1\" >" , f.text("a" ) );
