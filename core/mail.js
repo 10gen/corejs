b83a3c300a020a0ee401ab7a57e5c192d4fa9cbe
kristina <k@wooster.earthlink.net>
2008-07-23 09:59:41 -0400

    took out mail login info

--- core/mail.js
+++ core/mail.js
@@ -1,12 +1,12 @@
 /**
 *      Copyright (C) 2008 10gen Inc.
-*  
+*
 *    Licensed under the Apache License, Version 2.0 (the "License");
 *    you may not use this file except in compliance with the License.
 *    You may obtain a copy of the License at
-*  
+*
 *       http://www.apache.org/licenses/LICENSE-2.0
-*  
+*
 *    Unless required by applicable law or agreed to in writing, software
 *    distributed under the License is distributed on an "AS IS" BASIS,
 *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -143,11 +143,6 @@ Mail.SMTP.gmail = function( username , password ){
     return new Mail.SMTP( username , "smtp.gmail.com" , username , password , true , 465 );
 };
 
-
-// For now, IMAP is only being used with one account on gmail and only looking at the inbox.
-// When this changes, these variables will need to change:
-var username = "10gen.auto@gmail.com";
-var password = "jumpy171";
 
 /** @constructor Creates a new imap session (not connected)
  * @param {string} addr Address of email sender
