490890cddba2e3d6b7bdf1778e649b2781530e0b
kristina <k@wooster.earthlink.net>
2008-07-23 16:32:11 -0400

    spam

--- util/diff.js
+++ util/diff.js
@@ -1,12 +1,12 @@
 /**
 *      Copyright (C) 2008 10gen Inc.
-*  
+*
 *    Licensed under the Apache License, Version 2.0 (the "License");
 *    you may not use this file except in compliance with the License.
 *    You may obtain a copy of the License at
-*  
+*
 *       http://www.apache.org/licenses/LICENSE-2.0
-*  
+*
 *    Unless required by applicable law or agreed to in writing, software
 *    distributed under the License is distributed on an "AS IS" BASIS,
 *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -202,7 +202,6 @@ Util.Diff = {
         for(var prop in b){
             if(! (prop in a) ){
                 // add it
-                log("here!");
                 if(!d.add) d.add = {};
                 d.add[prop] = b[prop];
             }
