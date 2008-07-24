9cf41a667d6b62fddd5be9160d11f5de43dc35ab
kristina <k@wooster.earthlink.net>
2008-07-24 18:17:19 -0400

    fixed doc in random.js

--- util/random.js
+++ util/random.js
@@ -1,12 +1,12 @@
 /**
 *      Copyright (C) 2008 10gen Inc.
-*  
+*
 *    Licensed under the Apache License, Version 2.0 (the "License");
 *    you may not use this file except in compliance with the License.
 *    You may obtain a copy of the License at
-*  
+*
 *       http://www.apache.org/licenses/LICENSE-2.0
-*  
+*
 *    Unless required by applicable law or agreed to in writing, software
 *    distributed under the License is distributed on an "AS IS" BASIS,
 *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,14 +20,14 @@
  */
 var foo = {};
 
-/** Seeds and returns a Java random number generator.  
+/** Seeds and returns a Java random number generator.
  * See Java's java.util.Random documentation for more info.
- * 
+ *
  * @param {number} seed The seed to use
  * @return {random_generator} A seed random number generator.
  * @name random.getRandom
  *
- * Example: 
+ * @example
  *   var r = core.util.random().getRandom(42);
  *   r.nextInt();
  */
