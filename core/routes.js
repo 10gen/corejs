a964a49df56676a591320254d4b51cf91895c7f5
Mike Dirolf <mike@10gen.com>
2008-09-18 10:05:25 -0400

    debugging Routes.create: more info

--- core/routes.js
+++ core/routes.js
@@ -130,8 +130,12 @@ Routes.prototype.create = function() {
 	 */
 	var routes_path = function(the_scope) {
 		// get the path of the calling file relative to the site's path
-		var ap = app_path(the_scope);
-		var cp = calling_path(the_scope);
+		try {
+			var ap = app_path(the_scope);
+			var cp = calling_path(the_scope);
+		} catch (e) {
+			throw (e + " :: " + the_scope.debug());
+		}
 
 		var remainder = cp.substring(ap.length);
 
